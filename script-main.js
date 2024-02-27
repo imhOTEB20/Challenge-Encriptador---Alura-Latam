8f0eba2380cd418f60607a6cd4f2e711db68b85a
Cristian Geronimo <crisalbertoaa20@gmail.com>
2024-02-27 16:57:33 -0300

    Agrega la funcion desencriptar al archivo script-main.js y añade la funcionalidad para el boton encriptar.

--- script-main.js
+++ script-main.js
@@ -1,6 +1,7 @@
+//FUNCIONES
 function encriptar(texto){
     const VOCALES = 'aeiou';
-    const reemplazos = {
+    const REEMPLAZOS = {
         'a':'ai',
         'e':'enter',
         'i':'imes',
@@ -10,7 +11,7 @@ function encriptar(texto){
     let textoEncriptado = '';
     for (const LETRA of texto){
         if (VOCALES.includes(LETRA)){
-            textoEncriptado += reemplazos[LETRA];
+            textoEncriptado += REEMPLAZOS[LETRA];
         }
         else{
             textoEncriptado += LETRA;
@@ -18,8 +19,45 @@ function encriptar(texto){
     }
     return textoEncriptado;
 }
+function desencriptar(texto){
+    const REEMPLAZOS = {
+        'ai':'a',
+        'enter':'e',
+        'imes':'i',
+        'ober':'o',
+        'ufat':'u'
+    };
+    let textoDesencriptado = texto;
+
+    for (const CLAVE in REEMPLAZOS){
+        if (REEMPLAZOS.hasOwnProperty(CLAVE)) {
+            const VOCAL = REEMPLAZOS[CLAVE];
+            textoDesencriptado = textoDesencriptado.replace(CLAVE, VOCAL);
+        }
+    }
+    return textoDesencriptado;
+}
 
 function validarTexto(texto){
     const EXPRESIONR = /^[a-z\s]+$/u;
     return EXPRESIONR.test(texto);
 }
+//INTERACCION CON EL DOM
+document.getElementById('formulario').addEventListener('submit', function(event) {
+    event.preventDefault();
+});
+document.getElementById('btn-encriptar').addEventListener('click', function() {
+    const TEXTOINGRESADO = document.getElementById('campo_de_texto').value;
+
+    if (validarTexto(TEXTOINGRESADO)) {
+        const CAJARESULTADO = document.getElementById('caja_r');
+
+        CAJARESULTADO.innerHTML = '';
+        let resultado = document.createElement('p');
+        resultado.className = 'texto_resultado';
+        resultado.textContent = encriptar(TEXTOINGRESADO);
+        CAJARESULTADO.style.justifyContent = 'normal';
+        CAJARESULTADO.appendChild(resultado);
+    }
+
+});
