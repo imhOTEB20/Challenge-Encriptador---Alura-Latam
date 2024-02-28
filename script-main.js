8b5aa1e86c9b4328e70cbdba59e4961920d7a658
Cristian Geronimo <crisalbertoaa20@gmail.com>
2024-02-28 05:04:38 -0300

    Agrega el boton copiar y su correspondiente funcionalidad. Agrega el mensaje de advertencia.

--- script-main.js
+++ script-main.js
@@ -42,22 +42,106 @@ function validarTexto(texto){
     const EXPRESIONR = /^[a-z\s]+$/u;
     return EXPRESIONR.test(texto);
 }
-//INTERACCION CON EL DOM
+
+function transformar(tipo){
+    const CAMPODETEXTO = document.getElementById('campo_de_texto');
+    const CAJARESULTADO = document.getElementById('caja_r');
+    const TEXTOINGRESADO = CAMPODETEXTO.value;
+
+    if (validarTexto(TEXTOINGRESADO)) {
+        let contenedorTextoEncriptado = document.createElement('div');
+        let textoEncriptado = document.createElement('p');
+        let btnCopiar = document.createElement('button');
+        let contenedor_msg_error = document.querySelector('.contenedor_msg_error');
+        if (contenedor_msg_error){
+            contenedor_msg_error.remove();
+            CAMPODETEXTO.style.marginBottom = '40px';
+        }
+
+        btnCopiar.id = 'btn_copiar';
+        btnCopiar.innerText = 'Copiar';
+        btnCopiar.addEventListener('click', function() {
+            let resultado = document.querySelector('.texto_resultado');
+            const TEXTORESULTADO = resultado.textContent;
+            navigator.clipboard.writeText(TEXTORESULTADO).then(function(){
+            })
+            .catch(function(err) {
+                console.error('Error al copiar la transcripcion.');
+            });
+        });
+
+        contenedorTextoEncriptado.className = 'contenedor_texto_resultado';
+
+        CAJARESULTADO.innerHTML = '';
+        textoEncriptado.className = 'texto_resultado';
+        if (tipo === 'encriptar'){
+            textoEncriptado.textContent = encriptar(TEXTOINGRESADO);
+        }else{
+            textoEncriptado.textContent = desencriptar(TEXTOINGRESADO);
+        }
+
+        CAJARESULTADO.appendChild(contenedorTextoEncriptado);
+        contenedorTextoEncriptado.appendChild(textoEncriptado);
+        CAJARESULTADO.appendChild(btnCopiar);
+        CAMPODETEXTO.value = '';
+    }else{
+        if (TEXTOINGRESADO !== ''){
+            let contenedor_msg_error = document.querySelector('.contenedor_msg_error');
+            let imagenTextoVacio = document.getElementById('imagen_vacio');
+
+            if(!contenedor_msg_error){
+                contenedorMsgError = document.createElement('div');
+                contenedorMsgError.className = 'contenedor_msg_error';
+                contenedorMsgError.innerHTML = `
+                <img src="img/bi_exclamation-circle-fill.png" alt="">
+                <p>Solo letras minusculas y sin acento.</p>`;
+
+                CAMPODETEXTO.style.marginBottom = '0';
+                CAMPODETEXTO.insertAdjacentElement('afterend', contenedorMsgError);
+            }
+            if (imagenTextoVacio !== null){
+                let msgInfo = document.getElementById('msg_info');
+                msgInfo.textContent = 'El mensaje ingresado tiene un error';
+            }else{
+                CAJARESULTADO.innerHTML = '';
+                CAJARESULTADO.innerHTML = `
+                <img src="img/Muñeco.png" id="imagen_vacio" alt="">
+                <div class="resultado">
+                    <h3 id="msg_info">El mensaje ingresado tiene un error</h3>
+                    <p class="consejo">Ingresa el texto que desees encriptar o desencriptar</p>
+                </div>`;
+            }
+        }else{
+            let imagenTextoVacio = document.getElementById('imagen_vacio');
+            let contenedor_msg_error = document.querySelector('.contenedor_msg_error');
+
+            if (contenedor_msg_error){
+                contenedor_msg_error.remove();
+                CAMPODETEXTO.style.marginBottom = '40px';
+            }
+            if (imagenTextoVacio===null){
+                CAJARESULTADO.innerHTML = '';
+                CAJARESULTADO.innerHTML = `
+                <img src="img/Muñeco.png" id="imagen_vacio" alt="">
+                <div class="resultado">
+                    <h3 id="msg_info">Ningún mensaje fue encontrado</h3>
+                    <p class="consejo">Ingresa el texto que desees encriptar o desencriptar</p>
+                </div>`;
+            }else{
+                alert("pase por aqui");
+                let msgInfo = document.getElementById('msg_info');
+                msgInfo.textContent = 'Ningún mensaje fue encontrado';
+            }
+        }
+    }
+}
+//MANIPULACION DE BOTONES
 document.getElementById('formulario').addEventListener('submit', function(event) {
     event.preventDefault();
 });
 document.getElementById('btn-encriptar').addEventListener('click', function() {
-    const TEXTOINGRESADO = document.getElementById('campo_de_texto').value;
-
-    if (validarTexto(TEXTOINGRESADO)) {
-        const CAJARESULTADO = document.getElementById('caja_r');
-
-        CAJARESULTADO.innerHTML = '';
-        let resultado = document.createElement('p');
-        resultado.className = 'texto_resultado';
-        resultado.textContent = encriptar(TEXTOINGRESADO);
-        CAJARESULTADO.style.justifyContent = 'normal';
-        CAJARESULTADO.appendChild(resultado);
-    }
-
+    transformar('encriptar');
+});
+document.getElementById('btn-desencriptar').addEventListener('click', function() {
+    transformar('desencriptar');
 });
